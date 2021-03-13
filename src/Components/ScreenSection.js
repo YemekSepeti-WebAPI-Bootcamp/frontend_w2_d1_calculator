c11e20c0dd554bc98edeead428a5ccd808705330
mrturkay <muratturkaym@gmail.com>
2021-03-13 11:38:53 +0300

    screen section styling

--- src/Components/ScreenSection.js
+++ src/Components/ScreenSection.js
@@ -2,11 +2,30 @@ const styles = {
   screenSection: {
     width: "100%",
     height: "30%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
+  mainText: {
+    color: "#fff",
+    fontSize: 30,
+    padding: 10,
+  },
+  caption: {
+    color: "#999",
+    fontSize: 13,
+    paddingRight: 10,
   },
 };
 
 const ScreenSection = () => {
-  return <div style={styles.screenSection}></div>;
+  return (
+    <div style={styles.screenSection}>
+      <span style={styles.caption}>324+</span>
+      <span style={styles.mainText}>4231</span>
+    </div>
+  );
 };
 
 export default ScreenSection;
